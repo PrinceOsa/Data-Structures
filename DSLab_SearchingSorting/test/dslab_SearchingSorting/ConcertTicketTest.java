964c39ba85683d0acbe4f82eeccbf49bd183c4e1
PrinceOsa <54420091+PrinceOsa@users.noreply.github.com>
2020-08-28 04:46:57 -0400

    Add files via upload

---
+++ DSLab_SearchingSorting/test/dslab_SearchingSorting/ConcertTicketTest.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dslab_SearchingSorting;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kyle
+ */
+public class ConcertTicketTest {
+    
+    ConcertTicket ct1;
+    ConcertTicket ct2;
+    ConcertTicket ct3;
+    ConcertTicket ct4;
+    
+    public ConcertTicketTest() {
+        ct1 = new ConcertTicket("Niche Artist", 5);
+        ct2 = new ConcertTicket("Popular Artist", 6);
+        ct3 = new ConcertTicket("International Superstar", 7);
+        ct4 = new ConcertTicket("Pop Star", 7);
+    }
+
+    /**
+     * Test of compareTo method, of class ConcertTicket.
+     */
+    @Test
+    public void testCompareTo() {
+        System.out.println("compareTo");
+    
+        assertEquals(-1, ct1.compareTo(ct2));
+        assertEquals(0, ct3.compareTo(ct4));
+                assertEquals(1, ct2.compareTo(ct1));
+
+      
+    }
+    
+}
